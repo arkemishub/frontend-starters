8056f640447b0b7d6ff04031d06cb25a634f4eb1
Karim Hajouan <k.hajouan@arkemis.it>
2023-06-09 18:34:50 +0200

    chore: create a component for arke todo title

---
+++ examples/nextjs-todo/components/ui/ArkeTodoTitle.tsx
@@ -0,0 +1,14 @@
+const ArkeTodoTitle = () => {
+  return (
+    <>
+      <div className="flex items-center space-x-3">
+        <div className="w-6 h-6 border-y-[3px] border-black flex justify-center items-center">
+          <div className="w-1.5 h-1.5 bg-black rounded-full" />
+        </div>
+        <h1 className="text-3xl font-bold">ARKE todo</h1>
+      </div>
+    </>
+  );
+};
+
+export default ArkeTodoTitle;
